8e4e02eac856f050c369df9d9dc859b6c5518539
João Reis <jraposo@imaginarycloud.com>
2019-12-12 09:57:33 +0000

    Add router for application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginformComponent } from './loginform/loginform.component';
+import { SignupformComponent } from './signupform/signupform.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginformComponent },
+  { path: 'register', component: SignupformComponent },
+  { path: '', component: SignupformComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
