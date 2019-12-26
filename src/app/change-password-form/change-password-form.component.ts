7b5be59891162f3b855550fb32991d35244f9b13
João Reis <jraposo@imaginarycloud.com>
2019-12-26 12:56:31 +0000

    Add form validations

--- src/app/change-password-form/change-password-form.component.ts
+++ src/app/change-password-form/change-password-form.component.ts
@@ -24,33 +24,57 @@ export class ChangePasswordFormComponent implements OnInit {
     message: ""
   }
 
+  isCurrentPassInvalid: boolean = false
+  isNewPassInvalid: boolean = false
+  isNewPass1Invalid: boolean = false
+
+
   constructor(private loginService : LoginService,private router: Router) { }
 
   ngOnInit() {
   }
 
+  isCurrentPasswordInvalid() :boolean{
+    return this.currentpassword === undefined
+  }
+  isNewPasswordInvalid() :boolean{
+    return this.newpassword === undefined
+  }
+  isNewPassword1Invalid() :boolean{
+    return this.newpassword1 === undefined
+  }
+
+  isFormInvalid() :boolean{
+    return this.isCurrentPassInvalid || this.isNewPassInvalid
+            || this.isNewPass1Invalid
+  }
+
   changePassword(){
-    if(this.newpassword === this.newpassword1){
+    debugger
+    this.isCurrentPassInvalid = this.isCurrentPasswordInvalid()
+    this.isNewPassInvalid = this.isNewPasswordInvalid()
+    this.isNewPass1Invalid = this.isNewPassword1Invalid()
+
+    if(!this.isFormInvalid()){
+      if(this.newpassword !== this.newpassword1){
+        this.newPasswordError = {
+          active: true,
+          message: "New passwords fields didn't match"
+        }
+      }
+      else{
       this.loginService.changePassword(this.currentpassword,this.newpassword,this.newpassword1).
       subscribe(() => {
         this.router.navigate['/user']
       },(error) =>{
-        debugger
         if(error.response.data['old_password'] !== undefined){
           this.oldPasswordError = {
             active: true,
             message: "Your current password is wrong."
           }
-          console.log(this.oldPasswordError)
         }
       })
-    }
-    else {
-      this.newPasswordError = {
-        active: true,
-        message: "New passwords fields didn't match"
       }
-      console.log(this.newPasswordError)
     }
     }
 
