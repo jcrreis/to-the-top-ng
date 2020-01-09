66e9b1a3e0fd73209538b6173a6f8a8c8035bf5f
João Reis <jraposo@imaginarycloud.com>
2020-01-09 16:59:10 +0000

    Add changes to server.js and package.json

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/tothetop'));
+app.use(express.static('./dist/to-the-top-ng'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/tothetop/index.html'));
+res.sendFile(path.join(__dirname,'/dist/to-the-top-ng/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
