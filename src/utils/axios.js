3fa1a5468613f52fb121efdf20d4a945d17430f9
João Reis <jraposo@imaginarycloud.com>
2020-01-31 13:52:48 +0000

    testing

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -6,10 +6,8 @@ axios.defaults.withCredentials = true;
 
 axios.defaults.headers.common = {
   'X-Requested-With': 'XMLHttpRequest',
-  'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
   'New-Header': '123'
 
 };
 export default axios;
 
-X-CSRFTOKEN
