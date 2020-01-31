a156b3ede0e9b1c502d209d0f8d603c0fa76d0a7
João Reis <jraposo@imaginarycloud.com>
2020-01-31 13:47:21 +0000

    Change axios configs

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,4 +4,12 @@ import axios from 'axios'
 //axios.defaults.xsrfHeaderName = "CSRF-TOKEN"
 axios.defaults.withCredentials = true;
 
+axios.defaults.headers.common = {
+  'X-Requested-With': 'XMLHttpRequest',
+  'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+  'New-Header': '123'
+
+};
 export default axios;
+
+X-CSRFTOKEN
